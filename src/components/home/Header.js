8513be9b9394a9bd6ac821e7b7a94ea774a6b633
PeroHub <peterime600@gmail.com>
2022-05-13 14:48:05 +0100

    update

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,11 +1,13 @@
 import Box from '@mui/material/Box';
 import ButtonMain from "../ButtonMain"
-
+import ImageFrame from '../ImageFrame';
+import me from "../../images/Tshirt.png"
 function Header() {
     return ( 
         <Box sx={{border: "1px solid red"}}>
             Header
         <ButtonMain signup={"Register"} />
+        {/* <ImageFrame width="100px" height="100px" img={me} /> */}
         </Box>
      );
 }

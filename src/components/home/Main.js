f6a568ae69985f281aaa9f3ee3b79c77d01665a8
Nsisong <nsisongakpakpan01@gmail.com>
2022-05-29 23:45:43 +0100

    pero

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -296,10 +296,31 @@ function Main() {
                         <Card sx={{height:"17vh"}}>
                         <Typography mt={2} sx={{textAlign:"center", fontFamily:"Quando", color:"#333333", fontSize:"1rem", lineHeight:{md:"1.5rem"}, fontWeight:"bold"}}>{product.cetegory}</Typography>
                         <CardActions sx={{ width:"100%", display:"flex", flexDirection:"row", justifyContent:"space-between", alignContent:"end"}}>
-                        {/* <FavoriteBorderIcon sx={{width:"19%", height:"19px"}}/> */}
+                            <Button
+                                variant="outlined" 
+                                size="small"  
+                                sx={{width:"46%",
+                                borderRadius:"3px",
+                                textTransform:"none", 
+                                fontSize:"75%",
+                                mt:{md:3.5} }}
+                                startIcon={<FavoriteIcon sx={{color:"#B0B0B0"}}/>}>Save                             
+                            </Button>
+                                
+
+
                             
-                          
-                            <Button variant="outlined" size="small" disabled sx={{width:"46%", borderRadius:"3px", textTransform:"none", fontSize:"75%", mt:{md:3.5} }} startIcon={<ShoppingCartIcon sx={{color:"#B0B0B0"}}/>}> Add to Cart </Button>
+                            <Button 
+                                variant="outlined" 
+                                size="small"  
+                                sx={{width:"46%",
+                                borderRadius:"3px",
+                                textTransform:"none", 
+                                fontSize:"75%",
+                                mt:{md:3.5} }} 
+                                onClick={handleNavi}
+                                startIcon={<ShoppingCartIcon sx={{color:"#B0B0B0"}}/>}> Add to Cart 
+                            </Button>
                             
                             
                         </CardActions>
--- src/components/home/card.js
+++ src/components/home/card.js
@@ -1,7 +1,10 @@
-import {Container, Grid, Card, Box, Typography, Button} from '@mui/material';
+import {Container, Grid, Card, Box, Typography, Button, Avatar} from '@mui/material';
 // import { FormControl, InputLabel, Select, MenuItem} from '@mui/material'; 
 import ButtonMain from '../ButtonMain';
 import { useNavigate } from 'react-router-dom';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+
 function SavedProducts() {
     let result = localStorage.getItem('userWant')
     let final = JSON.parse(result)
@@ -11,13 +14,46 @@ function SavedProducts() {
     const handleNav = () => {
         navigate("/checkout")
     }
+
+    // <Button sx={{background: "red"}} variant="contained">REMOVE</Button>
+    // <Button>{final.price}</Button>
+
+    // <Box mt={5}>
+    // <ButtonMain variant={"contained"} size={"medium"} width={"10rem"} height={"45px"} mt={"5px"} text={"Procedd to Checkout"}/>
+    // <Button onClick={handleNav}>Proceed To Checkout</Button>
+    // </Box>
     return(
         <>
-            <Container maxWidth="lg" sx={{width:{xs:"100%", sm:"80%", md:"80%"}, mt: 8 }} >
-                <Grid container sx={{background:"#FAFCFD"}}>
-                    <Grid item xs={6} md={12} sx={{border:"1px solid red", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center", height:"60vh"}}>
-                        <Card sx={{display:"flex", height:"30vh", width:"60%"}}>
-                        <Box sx={{border:"1px solid blue", width:"100%", display:"flex", flexDirection:"row", justifyContent:"end"}}>    
+            <Container maxWidth="lg" sx={{width:{xs:"100%", sm:"80%", md:"80%"}}} >
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={12} sx={{border:"1px solid yellow", background:"#FAFCFD", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center", p:10 }}>
+                        <Card sx={{display:{xs:"block", md:"flex"}, height:{ xs:"70", md:"40vh"}, width:{xs:"80vw", sm:"80%", md:"90%"},border:"1px solid blue"}}>
+                            <Box sx={{ border:"1px solid red", height:{xs:"30vh", md:"inherit"}, width:{xs:"100%", md:"30vw"}, display:"flex", justifyContent:"center", alignItems:"center"}}>
+                                <Avatar sx={{height:"190px", width:"190px"}}><img src={final.image}/></Avatar>
+                            </Box>
+                            <Box sx={{ border:"1px solid red", height:"inherit", width:{xs:"100%", md:"60%"}}}>
+                                <TableContainer   sx={{overflow:"hidden",height:"100%", border:"6x solid yellow"}} >
+                                    <Table sx={{border:"6x solid red", height:"100%", display:"flex", flexDirection:"column" }}>
+                                        <TableHead sx={{border:"6x solid blue", width:"100%"}}>
+                                            <TableRow sx={{border:"6x solid blue", width:"100%"}}>
+                                                <TableCell sx={{borderBottom:"none"}}><Typography variant="h5" sx={{fontWeight:"3rem", border:"1px solid red"}}>FutureLabs Polo</Typography></TableCell>
+                                            </TableRow> 
+                                        </TableHead>
+                                        <TableBody sx={{border:"6x solid blue", height:"80%", width:"100%", display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
+                                            <TableRow sx={{border:"6x solid blue", width:"100%"}}>
+                                                <TableCell sx={{width:"100%", borderBottom:"none"}} align="left">(white)</TableCell>
+                                                <TableCell sx={{width:"100%", borderBottom:"none"}} align="right"><Button variant="outlined" size="3px"> 2</Button> </TableCell>
+                                            </TableRow>
+                                            <TableRow mt={50} >
+                                                <TableCell  align="left"><Button variant="text" sx={{color:"red", width:"100%", borderBottom:"none"}} size="50px">REMOVE</Button></TableCell>
+                                                <TableCell sx={{width:"100%", borderBottom:"none"}}  align="right"><Button variant="text"  size="50px">{final.price}</Button> </TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                                                            
+                            </Box>
+                        {/* <Box sx={{border:"1px solid blue", width:"100%", display:"flex", flexDirection:"row", justifyContent:"end"}}>    
                             <Box sx={{border:"1px solid red", width:"100%", display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
                                 <Box sx={{textAlign:"left"}}>
                                     <Typography variant="h5" sx={{fontWeight:"3rem", border:"1px solid red"}}>{final.cetegory}</Typography>
@@ -51,169 +87,19 @@ function SavedProducts() {
                                 
                             </Box>
                         
-                        </Box>
+                        </Box> */}
                         </Card>
-                        <Box mt={5}>
-                        <ButtonMain variant={"contained"} size={"medium"} width={"10rem"} height={"45px"} mt={"5px"} text={"Procedd to Checkout"}/>
-                        <Button onClick={handleNav}>Proceed To Checkout</Button>
+                        <Box pt={2} sx={{border:"1px solid green", width:"90%", height:"10vh", dislay:"flex", flexDirection:"column", justifyContent:"end", alignItems:"end"}}>
+                            <Button variant={"contained"} onClick={handleNav}>Proceed To Checkout</Button>
                         </Box>
 
 
                     </Grid>
-                    {/* <Grid item xs={6} md={2}> */}
-                    {/* <Stack spacing={8.5}>
-                                <Stack>
-                                    <Typography variant="p">Please Select:</Typography>
-                                    <Typography variant="h6">Color</Typography>
-                                    <Box>
-                                    <FormControl sx={{  minWidth: 120 }} size="small">
-                                        <InputLabel sx={{border:"#CCCCCC"}} >Color</InputLabel>
-                                        <Select
-                                            label="Age"
-                                        >
-                                            <MenuItem value="">
-                                            <em>None</em>
-                                            </MenuItem>
-                                            <MenuItem value={10}>Blue</MenuItem>
-                                            <MenuItem value={20}>White</MenuItem>
-                                            <MenuItem value={30}>Yellow</MenuItem>
-                                        </Select>
-                                        </FormControl>
-                                    </Box>
-                                </Stack>
-
-
-                                <Stack>
-                                    <Typography variant="h6">Size</Typography>
-                                    <Box>
-                                    <FormControl sx={{ minWidth: 120 }} size="small">
-                                        <InputLabel>Size</InputLabel>
-                                        <Select
-                                            label="Age"
-                                        >
-                                            <MenuItem value="">
-                                            <em>None</em>
-                                            </MenuItem>
-                                            <MenuItem value={10}>Small</MenuItem>
-                                            <MenuItem value={20}>Medium</MenuItem>
-                                            <MenuItem value={30}>Large</MenuItem>
-                                        </Select>
-                                        </FormControl>
-                                    </Box>
-                                </Stack>
-
-
-
-                                
-                                <Stack>
-                                    <Typography variant="h6">Quantity</Typography>
-                                    <Box>
-                                    <Stack direction="row" spacing={1} > <ButtonMain variant={"outlined"} border={"#CCCCCC"}   text={"-"} height={"25px"} width={"5px"} bg={"#FFFFFF"}/><ButtonMain variant={"text"} border={"#CCCCCC"} text={"1"} height={"25px"} width={"5px"} /><ButtonMain variant={"outlined"}  text={"+"} height={"25px"} width={"5px"} border={"#CCCCCC"} /></Stack>
-                                    </Box>
-                                </Stack>
-
-
-                            </Stack>
-                             */}
-
-{/* 
-                    </Grid> */}
+                    
                 </Grid>
 
 
-            {/* Second row */}
-            {/* <Grid container mt={5} sx={{background:"#FAFCFD"}}>
-                    <Grid item xs={6} md={8} sx={{border:"1px solid red", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center", height:"60vh"}}>
-                        <Card sx={{display:"flex", height:"30vh", width:"60%", display:"flex" }}>
-                        <Box sx={{border:"1px solid blue", width:"100%", display:"flex", flexDirection:"row", justifyContent:"end"}}>    
-                            <Box sx={{border:"1px solid red", width:"60%", display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
-                                <Box sx={{textAlign:"left"}}>
-                                    <Typography variant="h5" sx={{fontWeight:"3rem", border:"1px solid red"}}>FutureLabs Polo</Typography>
-                                    <Stack direction="row" spacing={15} sx={{border:"1px solid red"}}>
-                                        <Typography variant="p" sx={{fontWeight:"3rem"}}>(Black)</Typography>
-                                        <ButtonMain variant={"outlined"}  bg={"#FAFCFD"} text={"1"} height={"10px"} size={"3"} border={"#CCCCCC"} width={"0px"}/>
-                                    </Stack>
-
-                                </Box> 
-        
-
-                                <Box sx={{border:"1px solid red", width:"100%", display:"flex", alignItems:"end" }}>
-                                    <Stack direction="row" spacing={10}>
-                                        <ButtonMain variant={"text"} size={"medium"} width={"5rem"} height={"45px"} color={"red"} text={"REMOVE"}/>
-                                        <ButtonMain variant={"text"} size={"medium"} width={"5rem"} height={"45px"} color={"#949494"} text={"N2000"}/>
-
-                                    </Stack>
-                                </Box>
-                                
-                            </Box>
-                        
-                        </Box>
-                        </Card>
-                        <Box mt={5}>
-                        <ButtonMain variant={"contained"} size={"medium"} width={"10rem"} height={"45px"} mt={"5px"} text={"Add to Cart"}/>
-                        </Box>
-
-
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                    <Stack spacing={8.5}>
-                                <Stack>
-                                    <Typography variant="p">Please Select:</Typography>
-                                    <Typography variant="h6">Color</Typography>
-                                    <Box>
-                                    <FormControl sx={{  minWidth: 120 }} size="small">
-                                        <InputLabel sx={{border:"#CCCCCC"}} >Color</InputLabel>
-                                        <Select
-                                            label="Age"
-                                        >
-                                            <MenuItem value="">
-                                            <em>None</em>
-                                            </MenuItem>
-                                            <MenuItem value={10}>Blue</MenuItem>
-                                            <MenuItem value={20}>White</MenuItem>
-                                            <MenuItem value={30}>Yellow</MenuItem>
-                                        </Select>
-                                        </FormControl>
-                                    </Box>
-                                </Stack>
-
-
-                                <Stack>
-                                    <Typography variant="h6">Size</Typography>
-                                    <Box>
-                                    <FormControl sx={{ minWidth: 120 }} size="small">
-                                        <InputLabel>Size</InputLabel>
-                                        <Select
-                                            label="Age"
-                                        >
-                                            <MenuItem value="">
-                                            <em>None</em>
-                                            </MenuItem>
-                                            <MenuItem value={10}>Small</MenuItem>
-                                            <MenuItem value={20}>Medium</MenuItem>
-                                            <MenuItem value={30}>Large</MenuItem>
-                                        </Select>
-                                        </FormControl>
-                                    </Box>
-                                </Stack>
-
-
-
-                                
-                                <Stack>
-                                    <Typography variant="h6">Quantity</Typography>
-                                    <Box>
-                                    <Stack direction="row" spacing={1} > <ButtonMain variant={"outlined"} border={"#CCCCCC"}   text={"-"} height={"25px"} width={"5px"} bg={"#FFFFFF"}/><ButtonMain variant={"text"} border={"#CCCCCC"} text={"1"} height={"25px"} width={"5px"} /><ButtonMain variant={"outlined"}  text={"+"} height={"25px"} width={"5px"} border={"#CCCCCC"} /></Stack>
-                                    </Box>
-                                </Stack>
-
-
-                            </Stack>
-                            
-
-
-                    </Grid>
-                </Grid> */}
+         
 
 
 
